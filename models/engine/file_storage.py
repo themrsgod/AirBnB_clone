2eb880565e051c066fc161b5a3f23f1b73fc3d56
Moses Kisya <kishea.dev@gmail.com>
2023-05-15 02:57:59 +0000

    updated file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,77 +1,84 @@
 #!/usr/bin/python3
-
+"""Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
 from models.user import User
-from models.amenity import Amenity
+from models.state import State
 from models.city import City
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
-from models.state import State
 
 
 class FileStorage:
-    """
-    Represents file storage class
+    """Represents a storage engine for objects in a JSON file.
 
-    This class handles the storage and retrieval of objects to and from a JSON file.
+    The FileStorage class abstracts the implementation of storing and retrieving
+    objects from a JSON file. It provides a dictionary-like interface to store
+    and access objects by their IDs.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
     """
 
-    # Class variable to store the path to the JSON file
     __file_path = "file.json"
-
-    # Class variable to store the dictionary of objects
     __objects = {}
 
     def all(self):
-        """ Returns a dictionary of objects
+        """Return the dictionary of objects in the storage.
+
+        This method returns a dictionary of all objects currently stored in the
+        storage, where each key is in the format <ClassName>.<object_id>.
 
         Returns:
-            A dictionary of all objects currently stored in the file. The dictionary keys are strings that combine 
-            the name of the class and the object's `id` attribute.
+            dict: A dictionary of all objects in the storage.
         """
         return FileStorage.__objects
 
     def new(self, obj):
-        """ Adds a new object to the `__objects` dictionary
+        """Add a new object to the storage.
+
+        This method adds a new object to the storage, where the key is in the
+        format <ClassName>.<object_id>.
 
         Args:
-            obj: The object to be added to the dictionary
+            obj (BaseModel): The object to add to the storage.
         """
-        # Generate a key for the object by combining the name of the class and the object's `id` attribute
-        classname = obj.__class__.__name__
-        key = "{}.{}".format(classname, obj.id)
-
-        # Add the object to the dictionary using the generated key
-        FileStorage.__objects[key] = obj
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
-        """ Saves all objects in the `__objects` dictionary to a JSON file """
+        """Serialize objects in the storage to the JSON file.
 
-        # Convert all objects to dictionaries using their `to_dict()` method
-        object_dictionary = FileStorage.__objects
-        json_object = {obj: object_dictionary[obj].to_dict(
-        ) for obj in object_dictionary.keys()}
+        This method saves all objects in the storage to a JSON file specified by
+        __file_path. The objects are serialized as dictionaries and saved to the
+        file.
 
-        # Write the dictionary of objects to the JSON file
-        with open(FileStorage.__file_path, "w") as file_opened:
-            json.dump(json_object, file_opened)
+        Note:
+            The JSON file is overwritten with the current contents of the storage.
+        """
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
 
     def reload(self):
-        """ Loads all objects from the JSON file back into memory as objects of their respective classes """
+        """Deserialize objects from the JSON file into the storage.
 
+        This method deserializes objects from the JSON file specified by
+        __file_path into the storage. The objects are deserialized from
+        dictionaries and instantiated as objects.
+
+        Note:
+            If the JSON file does not exist, this method does nothing.
+        """
         try:
-            # Load the dictionary of objects from the JSON file
-            with open(FileStorage.__file_path, "r") as file_opened:
-                object_dictionary = json.load(file_opened)
-
-                # Create new objects of the correct class for each dictionary of object attributes in the loaded file
-                for o in object_dictionary.values():
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
                     cls_name = o["__class__"]
                     del o["__class__"]
                     self.new(eval(cls_name)(**o))
-
         except FileNotFoundError:
-            # Handle the case where the JSON file doesn't exist yet
-            print('Exception occured')
             return
